6aed301e731918d4320a30a20018d09999b2af75
nursultanamanzholdev <nursultanamanzholdev@gmail.com>
2025-04-16 16:39:36 +0500

    add config 2 benchmark=False

--- ViT PyTorch/train.py
+++ ViT PyTorch/train.py
@@ -63,7 +63,7 @@ try:
 except ImportError:
     has_wandb = False
 
-torch.backends.cudnn.benchmark = True
+torch.backends.cudnn.benchmark = False
 _logger = logging.getLogger('train')
 
 # The first arg parser parses out only the --config argument, this argument is used to
---
+++ ViT PyTorch/train_config2.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# This is a slightly modified version of timm's training script
+""" ImageNet Training Script
+
+This is intended to be a lean and easily modifiable ImageNet training script that reproduces ImageNet
+training results with some of the latest networks and training techniques. It favours canonical PyTorch
+and standard Python style over trying to be able to 'do it all.' That said, it offers quite a few speed
+and training result improvements over the usual PyTorch example scripts. Repurpose as you see fit.
+
+This script was started from an early version of the PyTorch ImageNet example
+(https://github.com/pytorch/examples/tree/master/imagenet)
+
+NVIDIA CUDA specific speedups adopted from NVIDIA Apex examples
+(https://github.com/NVIDIA/apex/tree/master/examples/imagenet)
+
+Hacked together by / Copyright 2020 Ross Wightman (https://github.com/rwightman)
+"""
+import argparse
+import time
+import yaml
+import os
+import logging
+from collections import OrderedDict
+from contextlib import suppress
+from datetime import datetime
+
+import torch
+import torch.nn as nn
+import torchvision.utils
+from torch.nn.parallel import DistributedDataParallel as NativeDDP
+
+from timm.data import create_dataset, create_loader, resolve_data_config, Mixup, FastCollateMixup, AugMixDataset
+from timm.models import create_model, safe_model_name, resume_checkpoint, load_checkpoint, model_parameters
+from timm.models.layers import convert_splitbn_model
+from timm.utils import *
+from timm.loss import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy, JsdCrossEntropy
+from timm.optim import create_optimizer_v2, optimizer_kwargs
+from timm.scheduler import create_scheduler
+from timm.utils import ApexScaler, NativeScaler
+
+from src import *
+
+try:
+    from apex import amp
+    from apex.parallel import DistributedDataParallel as ApexDDP
+    from apex.parallel import convert_syncbn_model
+
+    has_apex = True
+except ImportError:
+    has_apex = False
+
+has_native_amp = False
+try:
+    if getattr(torch.cuda.amp, 'autocast') is not None:
+        has_native_amp = True
+except AttributeError:
+    pass
+
+try:
+    import wandb
+
+    has_wandb = True
+except ImportError:
+    has_wandb = False
+
+torch.backends.cudnn.benchmark = False
+_logger = logging.getLogger('train')
+
+# The rest of the original train.py file follows
+# (unchanged from the original)
